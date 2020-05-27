7e12b48a602b1f7030b36bf7367875edf4ab8ba2
Mojmír Hartman <53128772+MojmirHartman@users.noreply.github.com>
2020-05-27 10:04:41 +0200

    Update tweak.bat

--- tweak.bat
+++ tweak.bat
@@ -1,2 +1,7 @@
 :: Remove bloatware
 powershell -ExecutionPolicy Bypass -File "%~dp0scripts\bloatware.ps1"
+
+:: Restrictions on automatic download and installation of bloatware
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338388Enabled" /t REG_DWORD /d "0" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338389Enabled" /t REG_DWORD /d "0" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
