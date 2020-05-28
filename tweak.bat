a1fb19b1b17020294034bdf188a8ece1382834b1
Mojmír Hartman <53128772+MojmirHartman@users.noreply.github.com>
2020-05-28 10:55:01 +0200

    Update tweak.bat

--- tweak.bat
+++ tweak.bat
@@ -5,3 +5,5 @@ powershell -ExecutionPolicy Bypass -File "%~dp0scripts\bloatware.ps1"
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338388Enabled" /t REG_DWORD /d "0" /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v "SubscribedContent-338389Enabled" /t REG_DWORD /d "0" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
+
+pause
